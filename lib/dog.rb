69624e9cbced705b65d67ac97c3e4a794c28f2a5
Jason Crescenzo <crescenzojay@gmail.com>
2018-10-30 15:49:30 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 class Dog 
-  def Dogname=(name)
-    @this_dogs_name = name
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
   end
  
-  def Dogname
+  def name
     @this_dogs_name
   end
-end 
-
-lassie = Dog.new
-lassie.Dogname = "Lassie"
+end
  
-puts lassie.Dogname
+lassie = Dog.new
+lassie.name = "Lassie"
+ 
+puts lassie.name
